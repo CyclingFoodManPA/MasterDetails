35d3bc205867694f536523e36a8fcd5029172ec7
CyclingFoodmanPA <ks.safford@verizon.net>
2021-05-29 20:18:51 -0400

    Completed tutorial, added code for HomeController, _Layout.cshtml, and Index.cshtml for the HomeController to display the OrderMaster and OrderDetails.

---
+++ MasterDetails/Models/OrderDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterDetails.Models
+{    
+    public class OrderDetailsViewModel
+    {
+        public System.Guid DetailId { get; set; }
+        public System.Guid MasterId { get; set; }
+        public string ProductName { get; set; }
+        public string Quantity { get; set; }
+        public string Amount { get; set; }
+    }
+}
---
+++ MasterDetails/Models/OrderViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MasterDetails.Context;
+
+
+namespace MasterDetails.Models
+{
+    public class OrderViewModel
+    {
+        public OrderViewModel()
+        {
+            this.OrderDetails = new List<OrderDetailsViewModel>();
+        }
+
+        public System.Guid MasterId { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+
+        public virtual ICollection<OrderDetailsViewModel> OrderDetails { get; set; }
+    }
+}
